63edf4e786585a02a869a81cce45498a1007f1b4
Peter Bata <peterbata@hotmail.com>
2016-11-05 16:43:19 -0400

    NOV052016

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Rocks
                 </div>
 
                 <div class="links">
